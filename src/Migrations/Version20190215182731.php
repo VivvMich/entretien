0bc79bcc26ae52c2bb80030ba331a0ff6221b169
VivvMich <mvicorp@hotmail.fr>
2019-02-15 19:27:58 +0100

    Commit Forfh

---
+++ src/Migrations/Version20190215182731.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190215182731 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comments CHANGE user_id user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE news CHANGE user_id user_id INT NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comments CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE news CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+    }
+}
